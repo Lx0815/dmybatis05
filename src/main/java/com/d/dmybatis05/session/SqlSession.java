e12984f88d36596279b5309494b1c22004a0f3a3
lxvae0815 <1507410636@qq.com>
2023-07-21 23:58:16 +0800

    feat: 新增事务管理的操作

--- src/main/java/com/d/dmybatis05/session/SqlSession.java
+++ src/main/java/com/d/dmybatis05/session/SqlSession.java
@@ -1,6 +1,8 @@
 package com.d.dmybatis05.session;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Objects;
 
 /**
  * @description: 会话对象
@@ -15,7 +17,40 @@ public class SqlSession {
     private Connection connection;
 
     public SqlSession(Connection connection) {
+        Objects.requireNonNull(connection, "连接对象为 null");
         this.connection = connection;
     }
 
+    /**
+     * 提交事务
+     */
+    public void commit() {
+        try {
+            connection.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 回滚事务
+     */
+    public void rollback() {
+        try {
+            connection.rollback();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 关闭会话，即关闭连接
+     */
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
